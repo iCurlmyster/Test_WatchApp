8f541490aaf285ab9a1d349df428128e5e96430e
Stephen Stucky <stucky3@Stucky-Air.local>
2015-08-07 21:58:41 -0500

    added watch

---
+++ Watch-Thing-2.0/Watch-Thing-2.0 WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  Watch-Thing-2.0 WatchKit Extension
+//
+//  Created by Stephen Stucky on 8/7/15.
+//  Copyright (c) 2015 Stephen Stucky. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ Watch-Thing-2.0/Watch-Thing-2.0 WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Watch-Thing-2.0 WatchKit Extension
+//
+//  Created by Stephen Stucky on 8/7/15.
+//  Copyright (c) 2015 Stephen Stucky. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
