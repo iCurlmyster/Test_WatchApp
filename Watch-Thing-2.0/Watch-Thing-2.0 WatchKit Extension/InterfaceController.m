3464d4cfe2f9860fefbab64d8df2182a4cfa596f
Stephen Stucky <stucky3@Stucky-Air.attlocal.net>
2015-08-09 00:12:24 -0500

    added cute kitten animation with some buttons

--- Watch-Thing-2.0/Watch-Thing-2.0 WatchKit Extension/InterfaceController.m
+++ Watch-Thing-2.0/Watch-Thing-2.0 WatchKit Extension/InterfaceController.m
@@ -10,16 +10,34 @@
 
 
 @interface InterfaceController()
+@property (weak, nonatomic) IBOutlet WKInterfaceButton *animationButton;
+@property (weak, nonatomic) IBOutlet WKInterfaceImage *animationFrame;
 
 @end
 
 
 @implementation InterfaceController
 
+int count = 0;
+
+- (IBAction)showAnimation {
+    if(count % 2 == 0) {
+        [ _animationFrame setHidden:NO];
+        _animationButton.title = @"Hide Animation";
+    } else {
+         [_animationFrame setHidden:YES];
+         _animationButton.title = @"Show Animation";
+    }
+    
+    count = count + 1;
+}
+
+
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    [ _animationFrame setHidden:YES];
 }
 
 - (void)willActivate {
